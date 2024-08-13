804cac195d9fff5cadcc2ce3ef0819bb9e004aa0
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2024-08-13 18:08:24 +0530

    Conversions

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
+// variable declaration
+
 console.log("Welcome to the world of JSSS")
+const accountId=1444342;
+let accountEmail="abc@gmail.com";
+var accountpassword="12345"
+accountcity="Jaipur"
+
+let accountstate;
+
+// accountId=111111;
+
+console.table([accountId,accountEmail,accountpassword,accountcity,accountstate])
+
+
+// datatypes,typeof and ecma script documentation
+
+
+
+
+"use strict"
+console.log(33+3)
+console.log(typeof(null))
+console.log(typeof(undefined))
+console.log("\t\t\t")
+
+// Conversions and operations in jS
+
+let variable="232fsega" // 33abc , null, "null" ,undefined ,true, false ,"palak"
+let convertednumber=Number(variable)
+console.log(typeof variable)
+console.log(variable)
+console.log( typeof convertednumber)
+console.log(convertednumber)
+
+console.log("\t\t\t\t\t")
+
+// to boolean
+
+let boolval=0 //"","palak",23,0
+let convertedboolval=Boolean(boolval)
+console.log(typeof boolval)
+console.log(boolval)
+console.log( typeof convertedboolval)
+console.log(convertedboolval)
+
+
+
+console.log("\t\t\t\t\t\t\t\t\t")
+
+//to string
+let stringval=true
+let convertedstringval=String(stringval)
+console.log(typeof stringval)
+console.log(stringval)
+console.log( typeof convertedstringval)
+console.log(convertedstringval)
+
