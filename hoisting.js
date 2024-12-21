7fba01b45b8b705175f43c16b8c8bef9f87bf27b
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2024-12-21 23:57:07 +0530

    hoisting in js

---
+++ hoisting.js
@@ -0,0 +1,16 @@
+
+getname();
+console.log(x);
+console.log(getname);
+
+
+var x=7;
+
+function getname()  
+{
+    console.log("Namaste Javascipt");
+    console.log(x);
+}
+getname();
+console.log(x);
+console.log(getname);
---
+++ script0-1.js
@@ -0,0 +1,10 @@
+// vid 1
+
+const accountId = 144553
+let accountEmail="palakkumrawat05@gmail.com"
+var accountPassword="12345"
+accountCity="Indore"
+var accountCity;
+accountID=23432
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity]);
